96c787f95d23add9b5544f4df8ce6e4f25ea3015
hKhatri <hkhatri@logilite.com>
2024-10-23 21:42:08 +0530

    fix: wrong signed or unsigned label issue on encounter selection window.

--- interface/forms/LBF/php/lbf_select_encounter.php
+++ interface/forms/LBF/php/lbf_select_encounter.php
@@ -16,23 +16,25 @@ $section_id = isset($_REQUEST['section_id']) ? $_REQUEST['section_id'] : "";
 $formname = isset($_REQUEST['formname']) ? $_REQUEST['formname'] : "";
 $encounter = isset($_REQUEST['encounter']) ? $_REQUEST['encounter'] : "";
 
-function fetch_appt_signatures_data_byId($eid) {
-    if(!empty($eid)) {
-        $eSql = "SELECT FE.encounter, E.id, E.tid, E.table, E.uid, U.fname, U.lname, E.datetime, E.is_lock, E.amendment, E.hash, E.signature_hash 
-                FROM form_encounter FE 
-                LEFT JOIN esign_signatures E ON (case when E.`table` ='form_encounter' then FE.encounter = E.tid else  FE.id = E.tid END)
-                LEFT JOIN users U ON E.uid = U.id 
-                WHERE FE.encounter = ? 
-                ORDER BY E.datetime ASC";
-        $result = sqlQuery($eSql, array($eid));
-        return $result;
+function fetch_appt_signatures_data_byId($eids) {
+    $result = array();
+
+    if(!empty($eids)) {
+        $eSql = "SELECT FE.encounter, E.id, E.tid, E.table, E.uid, E.datetime, E.is_lock, E.amendment, E.hash, E.signature_hash FROM form_encounter FE JOIN esign_signatures E ON (case when E.`table` ='form_encounter' then FE.encounter = E.tid else FE.id = E.tid END) WHERE FE.encounter IN (". implode(",", $eids) .") ORDER BY E.datetime ASC";
+
+        //$result = sqlQuery($eSql, array());
+        $lres = sqlStatement($eSql, array());
+        while ($lrow = sqlFetchArray($lres)) {
+            $result[] = $lrow;
+        }
     }
-    return false;
+    return $result;
 }
 
 $result4 = sqlStatement("SELECT fe.encounter,fe.date,openemr_postcalendar_categories.pc_catname, us.fname, us.mname, us.lname FROM form_encounter AS fe left join openemr_postcalendar_categories on fe.pc_catid=openemr_postcalendar_categories.pc_catid  left join users AS us on fe.provider_id = us.id  WHERE fe.pid = ? AND fe.encounter != ? order by fe.date desc", array($pid, $encounter));
 
 $enounterList = array();
+$appt_encounters = array();
 while ($rowresult4 = sqlFetchArray($result4)) {
 	$encounter = isset($rowresult4['encounter']) ? $rowresult4['encounter'] : '';
 	$id = '';
@@ -47,9 +49,18 @@ while ($rowresult4 = sqlFetchArray($result4)) {
 	}
 
 	if(!empty($id)) {
+		if ($encounter != 0) {
+	        $appt_encounters[] = $encounter;
+	    }
+
 		$rowresult4['form_id'] = $id;
 		$enounterList[] = $rowresult4;
 	}
+}
+
+$eSignData = array();
+if (!empty($appt_encounters)) {
+    $eSignData = fetch_appt_signatures_data_byId($appt_encounters);
 }
 
 ?>
@@ -75,6 +86,7 @@ while ($rowresult4 = sqlFetchArray($result4)) {
 	<div class="encounterContainer">
 		<ul>
 			<?php 
+				if (!empty($enounterList)) {
 				foreach ($enounterList as $i => $item) {
 					$edate = isset($item['date']) ? date($dateFormat, strtotime($item['date'])) : '';
 					$cCat = isset($item['pc_catname']) ? $item['pc_catname'] : '';
@@ -83,20 +95,35 @@ while ($rowresult4 = sqlFetchArray($result4)) {
 						$pName = ' - '.$pName;
 					}
 
-					$signed = $item['signed'] === true ? 'Signed' : 'Unsigned';
+					$encounter_id = isset($item['encounter']) ? $item['encounter'] : '';
+					$signed = 'Unsigned';
+
+					foreach ($eSignData as $eData) {
+			            if ($eData['encounter'] == $encounter_id) {
+			                if($eData !== false && isset($eData['is_lock']) && $eData['is_lock'] == '1') {
+			                    $signed = 'Signed';
+			                    break;
+			                }
+			            }
+			        }
+
+					//$signed = $item['signed'] === true ? 'Signed' : 'Unsigned';
 					if(!empty($signed)) {
 						$signed = ' - <i>'.$signed.'</i>';
 					}
 
 					$titleLink = trim($edate .' '. $cCat.$pName.$signed);
-
-					$encounter_id = isset($item['encounter']) ? $item['encounter'] : '';
 					$form_id = isset($item['form_id']) ? $item['form_id'] : '';
 
 					?>
 					<li>
 						<a href="javascript: void(0)" onClick="selectEncounter('<?php echo $section_id; ?>', '<?php echo $encounter_id; ?>', '<?php echo $form_id; ?>', '<?php echo $pid; ?>')"><?php echo $titleLink; ?></a>
 					</li>
+					<?php
+				}
+				} else {
+					?>
+					<li><?php echo xlt('Not Results'); ?></li>
 					<?php
 				}
 			?>
